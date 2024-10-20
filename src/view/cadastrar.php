98925c582754fbbdecf572e833ea5100bc2603e8
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2024-10-20 20:19:46 -0300

    header aplicado

--- src/view/cadastrar.php
+++ src/view/cadastrar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar-se</title>
-    <link rel="stylesheet" href="./css/">
+    <link rel="stylesheet" href="./view/css/principal.css">
 </head>
 <body>
     <header>
